d5553fbfd5663b235de8dcd0b8d9ed4f8062dc5e
Joel Goodman <joel@braverymedia.co>
2018-11-13 14:04:15 -0600

    configure hero/cover image for headers

---
+++ template-parts/components/site-nav.php
@@ -0,0 +1,4 @@
+<nav id="site-navigation" class="main-navigation" role="navigation">
+  <button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Site Menu', 'brvry' ); ?></button>
+  <?php wp_nav_menu( array( 'theme_location' => 'site-menu', 'container' => 'nav', ) ); ?>
+</nav><!-- #site-navigation -->
