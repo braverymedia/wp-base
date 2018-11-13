643ce68c0350f266f86daa65810b347cbbd4d138
Joel Goodman <joel@braverymedia.co>
2018-11-13 14:25:16 -0600

    break meta out to its own partial for reuse

--- template-parts/components/entry-header.php
+++ template-parts/components/entry-header.php
@@ -9,30 +9,6 @@
  ?>
 
 <?php if ( ! is_page() ) : ?>
-<div class="entry-meta">
-  <?php brvry_posted_on(); ?>
-	<?php brvry_posted_by(); ?>
-	<?php
-  // @TODO add category list
-
-	// Edit post link.
-		edit_post_link(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers. */
-					__( 'Edit <span class="screen-reader-text">%s</span>', 'brvry' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			),
-			'<span class="edit-link">' . brvry_get_icon_svg( 'edit', 16 ),
-			'</span>'
-		);
-	?>
-</div><!-- .meta-info -->
+<?php get_template_part('template-parts/components/entry', 'meta'); ?>
 <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 <?php endif; ?>
--- template-parts/content/content.php
+++ template-parts/content/content.php
@@ -1,18 +1,30 @@
 <?php
 /**
- * @package brvry
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage brvry
+ * @since 1.0.0
  */
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php twentynineteen_post_thumbnail(); ?>
 	<header class="entry-header">
-		<?php the_title( sprintf( '<h1 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h1>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php brvry_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php endif; ?>
+		<?php
+		if ( is_sticky() && is_home() && ! is_paged() ) {
+			printf( '<span class="sticky-post">%s</span>', _x( 'Featured', 'post', 'twentynineteen' ) );
+		}
+		if ( is_singular() ) :
+			the_title( '<h1 class="entry-title">', '</h1>' );
+		else :
+			the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+		endif;
+		?>
+		<?php get_template_part('template-parts/components/entry', 'meta'); ?>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
